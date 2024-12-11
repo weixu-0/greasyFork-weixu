5832f832ee9c246646f55c4e45f6fc7c807e47d3
weixu-0 <15760583391@163.com>
2024-12-11 20:53:53 +0800

    world - new world

--- temp.js
+++ temp.js
@@ -2,7 +2,7 @@
 // @name         autologin 自动登录
 // @namespace    https://github.com/weixu-0/greasyFork-weixu
 // @version      0.1
-// @description  try to take over the world!
+// @description  自动登录
 // @author       Wx_
 // @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
@@ -13,5 +13,5 @@
     'use strict';
 
     // Your code here...
-    console.log("hello world")
+    console.log("hello new world")
 })();
