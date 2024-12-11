465e907a0b95d6eb17e520bf7786108af24d8730
weixu-0 <15760583391@163.com>
2024-12-11 20:05:22 +0800

    up first version

--- temp.js
+++ temp.js
@@ -1,1 +1,17 @@
-console.log("你好")
+// ==UserScript==
+// @name         autologin 自动登录
+// @namespace    https://github.com/weixu-0/greasyFork-weixu
+// @version      0.1
+// @description  try to take over the world!
+// @author       Wx_
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log("hello world")
+})();
