69ee728b37659921bb02a18b74127c88b9fde48c
weixu-0 <15760583391@163.com>
2024-12-11 20:09:19 +0800

    add license

---
+++ autologin.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         autologin 自动登录
+// @namespace    https://github.com/weixu-0/greasyFork-weixu
+// @version      0.1
+// @description  try to take over the world!
+// @author       Wx_
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @license      AGPL-3.0
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log("hello world")
+})();
