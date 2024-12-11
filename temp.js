823a5d76ebe15f9460d9f292151d23cb7bc7c971
weixu-0 <15760583391@163.com>
2024-12-11 20:54:38 +0800

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,11 +1,20 @@
 // ==UserScript==
 // @name         autologin 自动登录
 // @namespace    https://github.com/weixu-0/greasyFork-weixu
+<<<<<<< Updated upstream
 // @version      0.1
 // @description  自动登录
 // @author       Wx_
 // @match        *://*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+=======
+// @version      0.2
+// @description  try to take over the world!
+// @author       Wx_
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @license      AGPL-3.0
+>>>>>>> Stashed changes
 // @grant        none
 // ==/UserScript==
 
